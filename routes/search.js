f057799971a57316275cb3129558729e83395326
nurulislam-ot <nurul@orangetoolz.com>
2024-11-05 16:59:42 +0600

    feat: typescript added

--- routes/search.js
+++
@@ -1,7 +0,0 @@
-const search_controller = require('../controller/search')
-
-const router = require('express').Router()
-
-router.get('/search/trip', search_controller.search)
-
-module.exports = router
