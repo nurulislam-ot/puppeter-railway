a406be88bab39584d7699e68e03b1c5bcd78931e
nurulislam-ot <nurul@orangetoolz.com>
2025-05-24 17:45:36 +0600

    feat(ticket): refactor ticket buying logic and add selectors for improved maintainability
    - Introduced a new selector file to centralize CSS selectors used in the ticket buying process.
    - Updated the buy_ticket function to accept parameters for preferred train name, URL, seat count, and time range.
    - Implemented a function to check if the departure time is within the preferred time range.
    - Enhanced seat selection logic to randomly select multiple available seats based on the specified count.
    - Updated the example usage of the buy_ticket function with new parameters.

--- src/ticket.ts
+++ src/ticket.ts
@@ -1,9 +1,19 @@
 import puppeteer from "puppeteer-extra"
 import { ElementHandle } from "puppeteer"
+import { TZDate } from "@date-fns/tz"
+import {
+  ACTIVE_USER_SELECTOR,
+  BOOK_NOW_BUTTON_SELECTOR,
+  DEPARTURE_DATE_TIME_SELECTOR,
+  SEAT_AVAILABILITY_SELECTOR,
+  SEAT_CLASS_CONTAINER_SELECTOR,
+  SEAT_TYPE_SELECTOR,
+  TRAIN_NAME_SELECTOR,
+} from "./constant/selector"
 
 const config = {
-  phone: "01722266531",
-  password: "your_password",
+  phone: process.env.USER_LOGIN_PHONE_NUMBER ?? "",
+  password: process.env.USER_LOGIN_PASSWORD ?? "",
 }
 
 puppeteer.use(
@@ -40,10 +50,36 @@ interface TripObjectI {
 }
 
 interface BuyTicketParams {
-  TRAIN_NAME: string
+  PREFER_TRAIN_NAME: string
+  URL: string
+  SEAT_COUNT: number
+  PREFER_TIME_RANGE?: [string, string]
 }
 
-const buy_ticket = async ({ TRAIN_NAME }: BuyTicketParams) => {
+const isThisHourInPreferTimeRange = (
+  hour: Date,
+  prefer_time_range: [string, string]
+): boolean => {
+  const [start, end] = prefer_time_range.map((time) => {
+    const [h, m] = time.split(":").map(Number)
+    return new TZDate(
+      hour.getFullYear(),
+      hour.getMonth(),
+      hour.getDate(),
+      h,
+      m,
+      "Asia/Dhaka"
+    )
+  })
+  return hour >= start && hour <= end
+}
+
+const buy_ticket = async ({
+  URL,
+  SEAT_COUNT,
+  PREFER_TIME_RANGE,
+  PREFER_TRAIN_NAME,
+}: BuyTicketParams) => {
   const trip_obj = {} as TripObjectI
 
   try {
@@ -52,64 +88,75 @@ const buy_ticket = async ({ TRAIN_NAME }: BuyTicketParams) => {
     })
     const page = await browser.newPage()
 
-    await page.goto(
-      "https://eticket.railway.gov.bd/booking/train/search?fromcity=Dhaka&tocity=Santahar&doj=16-Mar-2025&class=S_CHAIR",
-      {
-        waitUntil: "networkidle2",
-      }
-    )
+    await page.goto(URL, {
+      waitUntil: "networkidle2",
+    })
 
     const trips = await page.$$("app-single-trip")
 
-    await Promise.all(
-      trips.map(async (trip) => {
-        const train_name = await trip.$eval(
-          ".trip-left-info h2",
+    const trips_promises = trips.map(async (trip) => {
+      const train_name = await trip.$eval(
+        TRAIN_NAME_SELECTOR,
+        (elem) => elem.innerText
+      )
+      const active_user = await trip.$eval(
+        ACTIVE_USER_SELECTOR,
+        (elem) => elem.innerText
+      )
+      const departure_date_time_string = await trip.$eval(
+        DEPARTURE_DATE_TIME_SELECTOR,
+        (element) => element.innerText
+      )
+      const departure_date_time = new TZDate(
+        departure_date_time_string,
+        "Asia/Dhaka"
+      )
+
+      if (PREFER_TIME_RANGE) {
+        const condition = isThisHourInPreferTimeRange(
+          departure_date_time,
+          PREFER_TIME_RANGE
+        )
+        console.log(condition, departure_date_time)
+      }
+
+      const seat_class_container = await trip.$$(SEAT_CLASS_CONTAINER_SELECTOR)
+
+      const seat_availability_obj: SeatAvailabilityI = {}
+      const book_now_buttons: BookNowButtonsI = {}
+
+      const seat_details_promises = seat_class_container.map(async (seat) => {
+        const book_now_el = await seat.$(BOOK_NOW_BUTTON_SELECTOR)
+        const seat_type = await seat.$eval(
+          SEAT_TYPE_SELECTOR,
           (elem) => elem.innerText
         )
-        const active_user = await trip.$eval(
-          ".trip-left-info .active-trip-users p span",
+
+        // seat_availability = seat_avl
+        const seat_availability = await seat.$eval(
+          SEAT_AVAILABILITY_SELECTOR,
           (elem) => elem.innerText
         )
 
-        const seat_class_container = await trip.$$(".single-seat-class")
-
-        const seat_availability_obj: SeatAvailabilityI = {}
-        const book_now_buttons: BookNowButtonsI = {}
-
-        await Promise.all(
-          seat_class_container.map(async (seat) => {
-            const book_now_el = await seat.$(".book-now-btn")
-            const seat_type = await seat.$eval(
-              "span.seat-class-name",
-              (elem) => elem.innerText
-            )
-
-            // seat_availability = seat_avl
-            const seat_availability = await seat.$eval(
-              "span.all-seats",
-              (elem) => elem.innerText
-            )
-
-            seat_availability_obj[seat_type] = seat_availability
-            book_now_buttons[seat_type] = book_now_el
-          })
-        )
-
-        trip_obj[train_name] = {
-          active_user,
-          seat_availability: seat_availability_obj,
-          book_now_buttons,
-        }
-
-        console.log(trip_obj)
-
-        return train_name
+        seat_availability_obj[seat_type] = seat_availability
+        book_now_buttons[seat_type] = book_now_el
       })
-    )
 
-    const S_CHAIR = trip_obj[TRAIN_NAME].book_now_buttons.S_CHAIR
-    if (!S_CHAIR) throw Error(`${TRAIN_NAME} S_CHAIR not found!`)
+      await Promise.all(seat_details_promises)
+
+      trip_obj[train_name] = {
+        active_user,
+        seat_availability: seat_availability_obj,
+        book_now_buttons,
+      }
+
+      return train_name
+    })
+
+    await Promise.all(trips_promises)
+
+    const S_CHAIR = trip_obj[PREFER_TRAIN_NAME].book_now_buttons.S_CHAIR
+    if (!S_CHAIR) throw Error(`${PREFER_TRAIN_NAME} S_CHAIR not found!`)
 
     await S_CHAIR.click()
 
@@ -147,7 +194,34 @@ const buy_ticket = async ({ TRAIN_NAME }: BuyTicketParams) => {
 
     // now select seat
     const free_seats = await page.$$("button.seat-available")
-    await free_seats[0].click()
+
+    // random seat selection
+    if (free_seats.length <= SEAT_COUNT) throw Error("No free seats available")
+    console.log(`Total Free Seats: ${free_seats.length}`)
+
+    const random_seat_selected_index_array = new Set<{
+      seat_index: number
+      seat_number: string
+    }>()
+    while (random_seat_selected_index_array.size < SEAT_COUNT) {
+      const random_index = Math.floor(Math.random() * free_seats.length)
+      const seat_number = await free_seats[random_index].evaluate(
+        (el) => el.innerText
+      )
+      random_seat_selected_index_array.add({
+        seat_index: random_index,
+        seat_number,
+      })
+    }
+
+    console.log(random_seat_selected_index_array)
+
+    return
+    random_seat_selected_index_array.forEach(async (seat) => {
+      await free_seats[seat.seat_index].click()
+    })
+
+    return
     await delay(1000)
 
     const continue_btn = await page.waitForSelector("button.continue-btn", {
@@ -164,5 +238,8 @@ const buy_ticket = async ({ TRAIN_NAME }: BuyTicketParams) => {
 }
 
 buy_ticket({
-  TRAIN_NAME: "PANCHAGARH EXPRESS (793)",
+  PREFER_TRAIN_NAME: "DRUTOJAN EXPRESS (757)",
+  URL: "https://eticket.railway.gov.bd/booking/train/search?fromcity=Dhaka&tocity=Santahar&doj=27-May-2025&class=S_CHAIR",
+  SEAT_COUNT: 3,
+  PREFER_TIME_RANGE: ["07:45", "12:30"],
 })
